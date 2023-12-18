1e37ffa7b1b84c992f5d4ca90aa53ca8b96ec370
grbeno <grbeno@gmail.com>
2023-12-18 14:09:04 +0100

    deploy

--- config/settings.py
+++ config/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@rb=1xp@auyk5p910if525z)4*rv(c=2@es4-oso=8pr)f_9m$'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 
 # Application definition
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',  # whitenoise
     'django.contrib.staticfiles',
+
 
     # Third party apps
     'rest_framework',
@@ -56,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # corsheaders
     'django.middleware.common.CommonMiddleware',
@@ -131,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ str(BASE_DIR.joinpath('frontend/build', 'static'))  ]
+#STATICFILES_DIRS = [ str(BASE_DIR.joinpath('frontend/build', 'static'))  ]
+
+STATIC_ROOT = str(BASE_DIR.joinpath('frontend/build', 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -142,3 +147,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
+
+#CSRF_TRUSTED_ORIGINS = ['https://']
+
